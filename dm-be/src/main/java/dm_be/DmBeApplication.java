0d8b266941031ae607440509e2acc1a40ac0e3ce
Željko <zeljko.capan2905@gmail.com>
2024-10-21 15:06:16 +0200

    Backend za DisasterMaster
    Napravljen folder u kojem se radi backend

---
+++ dm-be/src/main/java/dm_be/DmBeApplication.java
@@ -0,0 +1,13 @@
+package dm_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmBeApplication.class, args);
+	}
+
+}
---
+++ dm-be/src/test/java/dm_be/DmBeApplicationTests.java
@@ -0,0 +1,13 @@
+package dm_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
