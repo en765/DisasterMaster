0d12b0e28fe2744e70eeb50bc8e5d8a31510418e
Željko <zc55258@fer.hr>
2024-11-02 10:09:26 +0100

    Create Role.java
    napravljena klasa/entitet Role

---
+++ IzvorniKod/dm-be/src/main/java/dm_be/domain/Role.java
@@ -0,0 +1,37 @@
+package dm_be.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Role {
+
+    @Id
+    private String roleId;
+
+    @Column(nullable = false)
+    private String roleName;
+
+    public String getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(String roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "roleId='" + roleId + '\'' +
+                ", roleName='" + roleName + '\'' +
+                '}';
+    }
+}
