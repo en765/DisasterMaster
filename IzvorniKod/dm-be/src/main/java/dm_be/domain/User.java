c79200436e36e4701628f29de6a186a13dbd782b
Željko <zc55258@fer.hr>
2024-11-02 10:25:05 +0100

    Update User.java
    napravljena apstraktna klasa/entitet User

--- IzvorniKod/dm-be/src/main/java/dm_be/domain/User.java
+++ IzvorniKod/dm-be/src/main/java/dm_be/domain/User.java
@@ -1,4 +1,63 @@
 package dm_be.domain;
 
-public class User {
+import jakarta.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class User {
+
+    @Id
+    @Column(name = "userId", unique = true, nullable = false)
+    private String userId;
+
+    @Column(name = "roleId", nullable = false)
+    private String roleId;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(String roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId='" + userId + '\'' +
+                ", roleId='" + roleId + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
