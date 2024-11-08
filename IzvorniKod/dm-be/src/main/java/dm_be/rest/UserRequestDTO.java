115a9430b3024123ee2c8aeda3021e0f10b8dad9
Željko <zc55258@fer.hr>
2024-11-08 09:47:24 +0100

    Update UserRequestDTO.java

--- IzvorniKod/dm-be/src/main/java/dm_be/rest/UserRequestDTO.java
+++ IzvorniKod/dm-be/src/main/java/dm_be/rest/UserRequestDTO.java
@@ -1,24 +1,96 @@
 package dm_be.rest;
 
 public class UserRequestDTO {
-
     private String userType;
     private String userId;
     private String email;
     private String password;
     private String role;
 
-    // Specifični atributi za Citizen
-    private String firstName;
-    private String lastName;
-    private Boolean isAnonymous;
+    private String firstName;         // Specifično za Citizen
+    private String lastName;          // Specifično za Citizen
+    private Boolean isAnonymous;      // Specifično za Citizen
+    private String organizationName;  // Specifično za HumOrg
+    private String governmentName;    // Specifično za Government
 
-    // Specifični atributi za HumOrg
-    private String organizationName;
+    // Getteri i setteri za sva polja
+    public String getUserType() {
+        return userType;
+    }
 
-    // Specifični atributi za Government
-    private String governmentName;
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
 
-    // Getteri i setteri
-    // ...
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Boolean getIsAnonymous() {
+        return isAnonymous;
+    }
+
+    public void setIsAnonymous(Boolean isAnonymous) {
+        this.isAnonymous = isAnonymous;
+    }
+
+    public String getOrganizationName() {
+        return organizationName;
+    }
+
+    public void setOrganizationName(String organizationName) {
+        this.organizationName = organizationName;
+    }
+
+    public String getGovernmentName() {
+        return governmentName;
+    }
+
+    public void setGovernmentName(String governmentName) {
+        this.governmentName = governmentName;
+    }
 }
