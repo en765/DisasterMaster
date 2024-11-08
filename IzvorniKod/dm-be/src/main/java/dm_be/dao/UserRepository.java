247e6490868fcd1cb283668c0cd1d932267b0327
Željko <zc55258@fer.hr>
2024-11-08 09:47:27 +0100

    Update UserRepository.java

--- IzvorniKod/dm-be/src/main/java/dm_be/dao/UserRepository.java
+++ IzvorniKod/dm-be/src/main/java/dm_be/dao/UserRepository.java
@@ -1,4 +1,16 @@
 package dm_be.dao;
 
-public class UserRepository {
+import dm_be.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    // Osnovne CRUD metode su naslijeđene iz JpaRepository
+    // Ovdje možete definirati dodatne metode po potrebi, npr.:
+
+    // Metoda za pronalaženje korisnika po email adresi
+    Optional<User> findByEmail(String email);
 }
