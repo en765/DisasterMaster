c29619e7edeb6199a8d972e9e8ddf6b9c49b6d32
Željko <zc55258@fer.hr>
2024-11-08 14:42:00 +0100

    Update UserServiceJpa.java

--- IzvorniKod/dm-be/src/main/java/dm_be/service/impl/UserServiceJpa.java
+++ IzvorniKod/dm-be/src/main/java/dm_be/service/impl/UserServiceJpa.java
@@ -2,8 +2,6 @@ package dm_be.service.impl;
 
 import dm_be.dao.UserRepository;
 import dm_be.domain.Citizen;
-import dm_be.domain.Government;
-import dm_be.domain.HumOrg;
 import dm_be.domain.User;
 import dm_be.rest.UserRequestDTO;
 import dm_be.service.UserService;
@@ -13,48 +11,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceJpa implements UserService {
 
-    private final UserRepository userRepo;
-
-    @Autowired
-    public UserServiceJpa(UserRepository userRepo) {
-        this.userRepo = userRepo;
-    }
 
     @Override
     public User createUser(UserRequestDTO userRequest) {
-        User user;
-
-        switch (userRequest.getUserType().toLowerCase()) {
-            case "citizen":
-                Citizen citizen = new Citizen();
-                citizen.setFirstName(userRequest.getFirstName());
-                citizen.setLastName(userRequest.getLastName());
-                citizen.setIsAnonymous(userRequest.getIsAnonymous());
-                user = citizen;
-                break;
-
-            case "humorg":
-                HumOrg humOrg = new HumOrg();
-                humOrg.setOrganizationName(userRequest.getOrganizationName());
-                user = humOrg;
-                break;
-
-            case "government":
-                Government government = new Government();
-                government.setGovernmentName(userRequest.getGovernmentName());
-                user = government;
-                break;
-
-            default:
-                throw new IllegalArgumentException("Unknown user type: " + userRequest.getUserType());
-        }
-
-        // Postavljanje općih atributa koji su zajednički za sve korisnike
-        user.setUserId(userRequest.getUserId());
-        user.setEmail(userRequest.getEmail());
-        user.setPassword(userRequest.getPassword());
-        user.setRoleId(userRequest.getRole());
-
-        return userRepo.save(user);
+        return null;
     }
 }
