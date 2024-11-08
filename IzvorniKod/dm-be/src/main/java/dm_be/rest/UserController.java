29c97b03a5db6383cb29acff6d07eacbc1322bbb
Željko <zc55258@fer.hr>
2024-11-08 09:42:36 +0100

    Update UserController.java

--- IzvorniKod/dm-be/src/main/java/dm_be/rest/UserController.java
+++ IzvorniKod/dm-be/src/main/java/dm_be/rest/UserController.java
@@ -1,4 +1,27 @@
 package dm_be.rest;
 
+import dm_be.domain.User;
+import dm_be.service.UserService;
+import dm_be.rest.UserRequestDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody UserRequestDTO userRequest) {
+        User createdUser = userService.createUser(userRequest);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
 }
