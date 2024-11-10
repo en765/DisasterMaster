e60cde156f0594de69415c2347c9e792bb04aa8d
Željko <zc55258@fer.hr>
2024-11-10 11:35:43 +0100

    Create AppUserRepository.java

---
+++ IzvorniKod/dm-be/src/main/java/dm_be/dao/AppUserRepository.java
@@ -0,0 +1,7 @@
+package dm_be.dao;
+
+import dm_be.domain.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+}
