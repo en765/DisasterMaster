3d9f7c4589a40a6f0fe8fa3f3fc5f894627f35a8
Željko <zc55258@fer.hr>
2024-11-10 10:53:28 +0100

    Update UserServiceJpa.java

--- IzvorniKod/dm-be/src/main/java/dm_be/service/impl/UserServiceJpa.java
+++ IzvorniKod/dm-be/src/main/java/dm_be/service/impl/UserServiceJpa.java
@@ -1,19 +1,21 @@
 package dm_be.service.impl;
 
-import dm_be.dao.UserRepository;
-import dm_be.domain.Citizen;
-import dm_be.domain.User;
-import dm_be.rest.UserRequestDTO;
-import dm_be.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import dm_be.dao.*;
+import dm_be.domain.*;
+import dm_be.service.*;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceJpa implements UserService {
 
+    private RoleRepository roleRepo;
+    private UserRepository userRepo;
+
 
     @Override
-    public User createUser(UserRequestDTO userRequest) {
-        return null;
+    public User createUser(User user, Long roleId) {
+        Role role = roleRepo.findById(roleId).orElseThrow(() -> new RuntimeException("Role not found"));
+    user.setRole(role);
+    return userRepo.save(user);
     }
 }
