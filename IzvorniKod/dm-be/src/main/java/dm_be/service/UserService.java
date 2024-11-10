daaf45985825ba2e58817f28fb9f5159a9c3be00
Željko <zc55258@fer.hr>
2024-11-10 10:53:30 +0100

    Update UserService.java

--- IzvorniKod/dm-be/src/main/java/dm_be/service/UserService.java
+++ IzvorniKod/dm-be/src/main/java/dm_be/service/UserService.java
@@ -3,6 +3,9 @@ package dm_be.service;
 import dm_be.domain.User;
 import dm_be.rest.UserRequestDTO;
 
+import java.util.List;
+
 public interface UserService {
-    User createUser(UserRequestDTO userRequest);
+    User createUser(User user, Long roleId);
+
 }
