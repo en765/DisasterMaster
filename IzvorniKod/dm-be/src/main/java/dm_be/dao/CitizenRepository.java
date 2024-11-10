34e90010cebee93730df4849b374d1a651403008
Željko <zc55258@fer.hr>
2024-11-10 10:53:15 +0100

    Create CitizenRepository.java

---
+++ IzvorniKod/dm-be/src/main/java/dm_be/dao/CitizenRepository.java
@@ -0,0 +1,7 @@
+package dm_be.dao;
+
+import dm_be.domain.Citizen;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CitizenRepository extends JpaRepository<Citizen, Long> {
+}
