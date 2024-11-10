ba0caab3c2a86d30dc1b02be82dc0ac948a41870
Željko <zc55258@fer.hr>
2024-11-10 10:57:51 +0100

    Update UserController.java

--- IzvorniKod/dm-be/src/main/java/dm_be/rest/UserController.java
+++ IzvorniKod/dm-be/src/main/java/dm_be/rest/UserController.java
@@ -8,17 +8,22 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.*;
+import dm_be.domain.*;
 
 import static org.springframework.web.servlet.function.ServerResponse.ok;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/users")
 public class UserController {
 
     private UserService userService;
 
-   @GetMapping("/")
-    public ResponseEntity<List<User>> getAllUsers() {
-       return ResponseEntity.ok().body(userService.getAllUsers());
-   }
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/citizen")
+    public Citizen createCitizen(@RequestBody Citizen citizen, @RequestParam Long roleId) {
+        return (Citizen) userService.createUser(citizen, roleId);
+    }
 }
