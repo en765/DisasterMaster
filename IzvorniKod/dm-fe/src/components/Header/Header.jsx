69b9e24944aae0598c39f740529c8a5ce2de7b35
NikolaMarinovic <nikola.marinovic@fer.hr>
2024-11-14 00:39:31 +0100

    update register

--- IzvorniKod/dm-fe/src/components/Header/Header.jsx
+++ IzvorniKod/dm-fe/src/components/Header/Header.jsx
@@ -4,23 +4,32 @@ import "./Header.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 
-function Header({ menuOpen, setMenuOpen, onLoginOpen }) {
-  return (
-    <header className="header">
-      <button className="menu-button" onClick={() => setMenuOpen(!menuOpen)}>
-        ☰
-      </button>
+function Header({ menuOpen, setMenuOpen, onLoginOpen, onRegisterOpen }) {
+    return (
+        <header className="header">
+            <button className="menu-button" onClick={() => setMenuOpen(!menuOpen)}>
+                ☰
+            </button>
 
-      <Link to="/" className="title-link">
-        <h1 className="title">DisasterMaster</h1>
-      </Link>
+            <Link to="/" className="title-link">
+                <h1 className="title">DisasterMaster</h1>
+            </Link>
 
-      <button className="login" onClick={onLoginOpen}>
-        <FontAwesomeIcon icon={faUser} style={{ marginRight: "8px" }} />
-        Log in
-      </button>
-    </header>
-  );
+
+            <div className="auth-buttons">
+                <button className="login" onClick={onLoginOpen}>
+                    <FontAwesomeIcon icon={faUser} style={{ marginRight: "8px" }} />
+                    Log in
+                </button>
+
+
+                <button className="register" onClick={onRegisterOpen}>
+                    <FontAwesomeIcon icon={faUser} style={{ marginRight: "8px" }} />
+                    Register
+                </button>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
