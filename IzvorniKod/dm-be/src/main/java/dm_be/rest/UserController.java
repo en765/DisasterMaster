024e903a4c6d75a353d82507ec570c94e65ffa02
Željko <zc55258@fer.hr>
2024-11-15 12:04:50 +0100

    Update UserController.java

--- IzvorniKod/dm-be/src/main/java/dm_be/rest/UserController.java
+++ IzvorniKod/dm-be/src/main/java/dm_be/rest/UserController.java
@@ -1,4 +1,4 @@
-package dm_be.rest;
+/*package dm_be.rest;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -6,4 +6,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 //@RequestMapping(path = "dm")
 public class UserController {
-}
+}*/
