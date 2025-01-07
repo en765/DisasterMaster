c7c02d9dc49e633a147be8a8ec0b45ee94c2fac7
Željko <zc55258@fer.hr>
2025-01-07 13:41:20 +0100

    Update SecurityConfiguration.java

--- IzvorniKod/dm-be/src/main/java/dm_be/security/SecurityConfiguration.java
+++ IzvorniKod/dm-be/src/main/java/dm_be/security/SecurityConfiguration.java
@@ -3,6 +3,8 @@ package dm_be.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,5 +20,10 @@ public class SecurityConfiguration {
             .anyRequest().authenticated(); // Svi ostali zahtevi zahtevaju autentifikaciju
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
