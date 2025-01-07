327f8eb8267f4f8596505631f877e0df2553fe83
Željko <zc55258@fer.hr>
2025-01-07 12:04:30 +0100

    normalno pokretanje h2 baze

---
+++ IzvorniKod/dm-be/src/main/java/dm_be/security/SecurityConfiguration.java
@@ -0,0 +1,22 @@
+package dm_be.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable() // Onemogući CSRF za H2 konzolu
+            .headers().frameOptions().disable() // Omogući H2 konzolu u iframe-u
+            .and()
+            .authorizeHttpRequests()
+            .requestMatchers("/h2-console/**").permitAll() // Dozvoli pristup H2 konzoli
+            .anyRequest().authenticated(); // Svi ostali zahtevi zahtevaju autentifikaciju
+
+        return http.build();
+    }
+}
