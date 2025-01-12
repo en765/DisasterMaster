9ce34449071a5d266e7c0a699ee3d8c242e669cb
Željko <zc55258@fer.hr>
2025-01-12 14:07:09 +0100

    Update SecurityConfig.java

--- IzvorniKod/dm-be/src/main/java/dm_be/security/SecurityConfig.java
+++ IzvorniKod/dm-be/src/main/java/dm_be/security/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -13,13 +15,20 @@ import java.util.Arrays;
 @EnableWebSecurity
 public class SecurityConfig {
 
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests()
-                .requestMatchers("/").permitAll()
-                .and()
-                .oauth2Login();
+            .authorizeHttpRequests()
+            .requestMatchers("/", "/login-success", "/h2-console/**", "/users/**").authenticated()
+            .anyRequest().authenticated()
+            .and()
+            .oauth2Login().defaultSuccessUrl("/login-success", true);
 
         return http.build();
 
